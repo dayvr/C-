7326da724fff09fd5c7ee537a941873d9c9f5709
Day Vazquez <dayvazquez@protonmail.com>
2022-08-14 11:41:59 +0200

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+using namespace std;
+
+/*
+To check if a number is a palindrome, first reverse the number. 
+If the reverse of the number is equal to the original number, the number is a palindrome, otherwise it is not.
+*/
+
+int main() {
+  int number = 121;
+  int reverse = 0, remainder = 0;
+  int tmp;
+  
+  tmp = number;
+  while (tmp != 0) {
+    remainder = tmp % 10; // Get the last digit of temp
+    reverse = reverse * 10 + remainder;
+    tmp = tmp / 10; // Remove the last digit
+  }
+  
+  if (number == reverse) {
+    cout << "is palindrome";
+  } else {
+    cout << "not a palindrome";
+  }
