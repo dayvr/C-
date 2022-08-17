651d4111a49b4ecda544e553e6237deff11d75bd
Day Vazquez <dayvazquez@protonmail.com>
2022-08-17 19:09:35 +0200

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,16 +3,24 @@
 using namespace std;
  
 // Recursive fibonacci
-int Fibonacci(int n) {
+int fibonacci(int n) {
   /*
   Each element in fibonacci is a sum of its previous two elements. 
   Recursively sum the last two elements until the base case.
   */
  
- // Base Case
- if (n <= 1) {
-   return n;
- }
- // Recursive Case
- return Fibonacci(n - 1) + Fibonacci(n - 2);
+  // Base Case
+  if (n <= 1) {
+    return n;
+  }
+  // Recursive Case
+  return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+int main () {
+  int result;
+
+  result = fibonacci (7);
+
+  cout << result << endl;
 }
