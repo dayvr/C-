c6580399ac258b701523db8a6d8aabdb77507dbd
Day Vazquez <dayvazquez@protonmail.com>
2022-08-20 17:54:40 +0200

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,31 +1,43 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Bubble Sort in descending order.
-void sort_elements(int arr[], int size) {
+// Bubble Sort in descending order
+void bubble_sort_descending(int arr[], int size) {
   int tmp;
   
   for (int i = 0; i < size; i++) {
     for (int j = i + 1; j < size; j++) {
       if (arr[i] < arr[j]) {
-        // Swap elements
+        // Swap elements using a temporal variable
         tmp = arr[j];
         arr[j] = arr[i];
         arr[i] = tmp;
       }
     }
   }
- }
+  for (int i = 0; i < size; i++) cout << arr[i] << ' ';
+}
 
- int main () {
-   int size = 5;
-   int arr[size] = {16, 2, 70, 98, 7};
+// Bubble Sort in ascending order
+void bubble_sort_ascending(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    for(int j = 1; j < size - i; j++) {
+      if (arr[j] < arr[j-1]) {
+        swap(arr[j], arr[j-1]); // Swap using built-in function
+      }
+    }
+  }
+  for (int i = 0; i < size; i++) cout << arr[i] << ' ';
+  cout << "\n";
+}
 
-   sort_elements (arr, 5);
+int main() {
+  int size = 6;
+  int arr[6] = {6, 3, 5, 4, 1, 2};
+  int arr2[6] = {6, 3, 5, 4, 1, 2};
 
-   for (int i = 0; i < size; i++) {
-     cout << arr[i] << ' ';
-   }
-   return 0;
- }
+  bubble_sort_ascending(arr2, size);
+  bubble_sort_descending(arr2, size);
+  
+  return 0;
+}
